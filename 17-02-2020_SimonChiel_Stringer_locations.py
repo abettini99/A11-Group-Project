ab0385699fc91d5b11eaef55cad4fd12bbc2882e
chielvanbaardwijk <60961080+chielvanbaardwijk@users.noreply.github.com>
2020-02-18 09:08:27 +0100

    Update 17-02-2020_SimonChiel_Stringer_locations.py

--- 17-02-2020_SimonChiel_Stringer_locations.py
+++ 17-02-2020_SimonChiel_Stringer_locations.py
@@ -4,10 +4,10 @@ def st_locations(C_a, h_a, n_st):
     s_semi = np.pi*h_a/4
     s_triangle = np.sqrt(C_a**2 - h_a*C_a + h_a**2/2)
     dsi = s/n_st
-    st = np.zeros((3,5))
-    st[0,:] = np.arange(dsi,s,dsi)
-    i = 0
     ds = 0
+    st = np.zeros((3,11))
+    st[0,:6] = np.arange(ds,s,dsi)
+    i = 0
     while ds < s_semi:
         theta = (ds/s_semi) * (np.pi/2)
         y = np.sin(theta)*(h_a/2)
@@ -18,17 +18,19 @@ def st_locations(C_a, h_a, n_st):
 
     i = 1
     ds = ds - s_semi
-    while ds < s-dsi:
+    while i < 6:
         alpha = np.arccos((C_a - (h_a/2))/s_triangle)
         y = np.sin(alpha) * (s_triangle - ds)
-        z = (C_a-(h_a/2)) - np.cos(alpha) * (s_triangle - ds)
+        z = C_a - np.cos(alpha) * (s_triangle - ds)
         st[1:,i] = y,z
         ds += dsi
         i += 1
       
-
+    while i < 11:
+        st[1:,i] = -st[1:,-i]
+        i +=1
     
-    return st, i, ds , s_semi
+    return st
 n_st = 6
 C_a = 0.515
 h_a = 0.248
