58013db6367807259aff3491e61496af500d9818
chielvanbaardwijk <60961080+chielvanbaardwijk@users.noreply.github.com>
2020-02-20 15:37:28 +0100

    Update calculator.py

--- 19-02-2020_Base/calculator.py
+++ 19-02-2020_Base/calculator.py
@@ -66,16 +66,12 @@ def integrator_cumutrap(f, x, x0):
     F = x0 + np.sum( ((x[1:] - x[:-1])/2) * (f[1:] + f[:-1]) )
     return F 
 
-def integrator_simp(f, x):
+def integrator_simp(f, x, a, b, dx):
     Flst = []
-    Flst.append( ((x[2::2] - x[:-2:2])/6) * (f[2::2] + 4*(f[1:-1:2]) + f[:-2:2]) )
+    Flst.append(  (f[2::2] + 4*(f[1:-1:2]) + f[:-2:2]) )
     Farr = np.array(Flst)
-    F = np.sum(Farr)
+    F = dx/3 * np.sum(Farr)
     return Farr, F
-
-def integrator_cumusimp(f, x):
-    F = np.sum( ((x[1:] - x[:-1])/6) * (f[1:] + 2*(f[1:]+f[:-1]) + f[:-1]) )
-    return F
 
 def st_locations(C_a, h_a, n_st):
     """
